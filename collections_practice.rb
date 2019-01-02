ff161e03e7ac656982aa21be6c43fa74b2bf4877
Ryan Gill <rdgill95@gmail.com>
2019-01-02 00:46:12 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,11 @@
-# your code goes here
+def begins_with_r(collection)
+  collection.all? do |string|
+    string[0] == "r"
+  end
+end
+
+def contain_a(collection)
+  collection.select do |x|
+    x.include?("a")
+  end
+end
